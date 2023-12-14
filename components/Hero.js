d2c6c3d6827877ecb05852af5e1b18a79a8791ac
Mallory Antomarchi <151766818+malloai@users.noreply.github.com>
2023-12-14 18:23:39 +0100

    Added initial files

---
+++ components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero--photo" />
+            <h1 className="hero--header">Expériences Online</h1>
+            <p className="hero--text">Rejoignez des expériences interactives uniques, dirigées par un de nos fantastiques hôtes, tout cela sans quitter le confort de votre chez vous.</p>
+        </section>
+    )
+}
---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        title: "FAQ avec Katie Zaferes",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Apprenez la photographie",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Session VTT en montagne",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norvège",
+        openSpots: 3,
+    }
+]
